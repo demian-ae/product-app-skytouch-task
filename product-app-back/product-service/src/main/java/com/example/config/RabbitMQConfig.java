a4030dcf3da7de99049fc5ae361fea1cc952497c
demian-ae <sergio.acuayte@encora.com>
2025-04-09 15:37:44 -0600

    Fix config classes

--- product-app-back/product-service/src/main/java/com/example/config/RabbitMQConfig.java
+++ product-app-back/product-service/src/main/java/com/example/config/RabbitMQConfig.java
@@ -2,11 +2,13 @@ package com.example.config;
 
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQConfig {
     @Value("${rabbitmq.queue.name}")
     private String queue;
@@ -23,15 +25,15 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public TopicExchange topicExchange() { 
-        return new TopicExchange(exchange);
+    public DirectExchange directExchange() { 
+        return new DirectExchange(exchange);
     }
 
     @Bean
     public Binding binding(){
         return BindingBuilder
             .bind(queue())
-            .to(topicExchange())
+            .to(directExchange())
             .with(routingKey);
     }
 
