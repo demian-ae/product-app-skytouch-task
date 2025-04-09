4667ac62e2367670cfa2a60552689cdf9a617955
demian-ae <sergio.acuayte@encora.com>
2025-04-09 16:55:17 -0600

    adding the json message converter to configuration

--- product-app-back/product-management/src/main/java/com/example/config/RabbitMQConfig.java
+++ product-app-back/product-management/src/main/java/com/example/config/RabbitMQConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,6 +39,18 @@ public class RabbitMQConfig {
             .bind(queue())
             .to(directExchange())
             .with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) { 
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter());
+        return template; 
     }
 
     /*
