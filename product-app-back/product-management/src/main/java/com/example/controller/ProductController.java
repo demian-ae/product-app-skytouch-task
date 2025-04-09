f38dc465a3f7fd9ab88e3c27b969b898828665ca
demian-ae <sergio.acuayte@encora.com>
2025-04-09 17:38:22 -0600

    deleting unused controller

--- product-app-back/product-management/src/main/java/com/example/controller/ProductController.java
+++ product-app-back/product-management/src/main/java/com/example/controller/ProductController.java
@@ -1,21 +1,25 @@
 package com.example.controller;
 
-import java.time.LocalDate;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.common.Product;
+import com.example.common.ProductResponse;
+import com.example.producer.RabbitMQProducer;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/v1/products")
 public class ProductController {
-    
-    @GetMapping
-    public String hello() { 
-        Product p = new Product(1L, "Jabon", "Jabon para manos", 12.0, LocalDate.now());
-        return p.toString();
+    private RabbitMQProducer rabbitMQProducer;
+
+    public ProductController(RabbitMQProducer rabbitMqProducer) {
+        this.rabbitMQProducer = rabbitMqProducer;
     }
 
+    @GetMapping
+    public ResponseEntity<ProductResponse> sendMessage() {
+        ProductResponse response = rabbitMQProducer.requestAllProducts();
+        return response != null ? ResponseEntity.ok(response) : ResponseEntity.notFound().build();
+    }
 }
