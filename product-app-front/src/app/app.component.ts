c3a2dba8bb38c327f77400a5d24449e89ba0c4f9
demian-ae <sergio.acuayte@encora.com>
2025-04-09 20:11:37 -0600

    adding layout

--- product-app-front/src/app/app.component.ts
+++ product-app-front/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductsListComponent, ProductFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
